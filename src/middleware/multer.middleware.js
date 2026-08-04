0997b91368dc3047a90c04c7af7dd723c70bfe59
Abhishek Pant <pantabhishek202@gmail.com>
2026-08-04 12:28:15 +0530

    Add multer and cloudinary packages

---
+++ src/middleware/multer.middleware.js
@@ -0,0 +1,23 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    //cb full-form CallBack
+    cb(null, './public/temp')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, file.originalname)
+    console.log(file.fieldname + '-' + uniqueSuffix);
+  }
+})
+
+ export const upload = multer({ 
+    storage,
+ })
+
+
+//  What is the differcne betwen export {storage} and export const upload = multer({ 
+//     storage,
+//  })
+// for this code 
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,33 @@
+import {v2 as cloudinary} from "cloudinary";
+import fs from "fs";
+
+// Configuration
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key:CLOUDINARY_API_KEY, 
+    api_secret: CLOUDINARY_API_sECRET
+
+});
+
+const uploadOnCloudinary = async(localFilePath)=>{
+    try{
+        if(!localFilePath) return null
+        //upload the file on Cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath,{
+            resource_type:"auto"
+        })
+        //The file has been uploaded successfully.
+       console.log("The file is uploaded successfully in the cloudinary .",response.url);
+       return response;
+    }catch(error){
+        fs.unlinkSync(localFilePath)
+        //remove the locally saved temporary file as the upload operation got failed 
+
+    }
+}
+
+
+export{uploadOnCloudinary}
+
+
+    
