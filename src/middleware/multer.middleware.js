8e53f9b3b042cff71dd46a67c3c7aa1504c0cac3
Abhishek Pant <pantabhishek202@gmail.com>
2026-08-15 20:29:37 +0530

    Add Changes

--- src/middleware/multer.middleware.js
+++ src/middleware/multer.middleware.js
@@ -1,11 +1,11 @@
 import multer from "multer";
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
+  destination: function (req, file, cb) {//destination :The folder to which the file has been saved 
     //cb full-form CallBack
     cb(null, './public/temp')
   },
-  filename: function (req, file, cb) {
+  filename: function (req, file, cb) { //The name of the File within the destination 
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
     cb(null, file.originalname)
     console.log(file.fieldname + '-' + uniqueSuffix);
