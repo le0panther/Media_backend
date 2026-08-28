44050865c38f6d1520d8c38d4025dc45459be611
Abhishek Pant <pantabhishek202@gmail.com>
2026-08-28 22:23:24 +0530

    Add changes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,7 @@ import { ApiError } from "../utils/apiErrors.js";
 import {User} from "../models/user.model.js";
 import {uploadOnCloudinary} from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/apiResponse.js";
+import jwt from "jsonwebtoken";
 
 
 const generateAccessAndRefreshTokens = async(userId)=>{
@@ -231,6 +232,35 @@ const logoutUser=asyncHandler(async(req,res)=>{
 
 })
 
+const refreshAccessToken = asyncHandler(async(req,res)=>{
+        const incomingRefreshToken=req.cookies.refreshToken || req.body.refreshToken
+
+        if(!incomingRefreshToken){
+                throw new ApiError(401,"Unauthorized request ")
+                //Unauthorized isliye bole kyuki humara token hi sahi nahi hai 
+
+                //ye step (jwt verify step ,i mean jwt verify karna important kyu hai  )
+
+                const decodedToken = jwt.verify(incomingRefreshToken,process.env.REFRESH_TOKEN_SECRET)
+
+                const user = await User.findById(decodedToken?._id)
+
+                if(!user){
+                        throw new ApiError(401,"Invalid Refresh Token ")
+                }
+
+
+                if(incomingRefreshToken != user?.refreshToken){
+                        throw new ApiError(401,"Refresh Token is expired or used ")
+                }
+
+
+
+                
+
+        }
+
+})
 
 export {
         registerUser,
